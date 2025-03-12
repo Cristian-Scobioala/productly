7aa15d40a0fabc60684c61ef058ff9785c87d7e4
tasosger <tassos.gerassis@gmail.com>
2025-03-13 01:40:36 +0200

    remove data.sql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product (id, name, description, price) VALUES (1, 'Laptop', 'High-performance laptop', 999.99);
-INSERT INTO product (id, name, description, price) VALUES (2, 'Smartphone', 'Latest model smartphone', 699.49);
-INSERT INTO product (id, name, description, price) VALUES (3, 'Headphones', 'Noise-canceling headphones', 199.99);
